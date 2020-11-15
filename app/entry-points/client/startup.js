02cd5a901682d50e9cc2aa6075d6762f5595124f
Code Bling <codeblingx@gmail.com>
2020-11-15 16:23:22 -0500

    Combine all 4 top-level elements into one

--- app/entry-points/client/startup.js
+++ app/entry-points/client/startup.js
@@ -108,41 +108,73 @@ async function renderAsync() {
 
   const history = createBrowserHistory();
 
+  const WrapWithRoutingSwitch = ({ component }) => (
+    <Switch>
+      {/* Map our locales to separate routes */}
+      { otherLocales.map(locale => (
+        <Route
+          key={locale}
+          path={`/${locale}/`}
+          render={props => <App component={component} {...props} locale={locale} section="app" />}
+        />
+      ))}
+
+      { primaryLocale && (
+        <Route
+          key={primaryLocale}
+          path="/"
+          render={props => <App component={component} {...props} locale={primaryLocale} section="app" />}
+        />
+      )}
+
+      {/* If no valid locale is given, we redirect to same route with the preferred locale prefixed */}
+      <Route render={({ location }) => <Redirect to={`/${window.__PREFERRED_LOCALE__ || otherLocales[0]}${location.pathname}`} />} />
+    </Switch>
+  );
+
   // Inject react app components into App's Shell
-  const ClientApp = ({ component }) => (
+  const ClientApp = () => (
     <Router history={history}>
       <ApolloProvider client={client}>
-        <Switch>
-          {/* Map our locales to separate routes */}
-          { otherLocales.map(locale => (
-            <Route
-              key={locale}
-              path={`/${locale}/`}
-              render={props => <App component={component} {...props} locale={locale} section="app" />}
-            />
-          ))}
+        <header className="fixed top-0 z1">
+          <div className="flex justify-between items-center">
+            <div className="flex justify-center items-center header__burger header__burger--show">
+              <svg className="menu__icon no--select" width="24px" height="24px" viewBox="0 0 48 48" fill="#fff">
+                <path d="M6 36h36v-4H6v4zm0-10h36v-4H6v4zm0-14v4h36v-4H6z" />
+              </svg>
+            </div>
+            <div id="header-title" className="center header__title no--select">
+              <WrapWithRoutingSwitch component={HeaderTitle} />
+            </div>
+            <div id="header-lang-picker" className="no--select">
+              <WrapWithRoutingSwitch component={LanguagePicker} />
+            </div>
+          </div>
+        </header>
 
-          { primaryLocale && (
-            <Route
-              key={primaryLocale}
-              path="/"
-              render={props => <App component={component} {...props} locale={primaryLocale} section="app" />}
-            />
-          )}
+        <div id="burger-btn-controller" />
 
-          {/* If no valid locale is given, we redirect to same route with the preferred locale prefixed */}
-          <Route render={({ location }) => <Redirect to={`/${window.__PREFERRED_LOCALE__ || otherLocales[0]}${location.pathname}`} />} />
-        </Switch>
+        <div className="fixed top-0 bottom-0 z2 menu">
+          <div className="menu__header" />
+          <ul id="menu" className="menu__list">
+            <WrapWithRoutingSwitch component={Menu} />
+          </ul>
+        </div>
+
+        <div className="fixed top-0 bottom-0 left-0 right-0 z1 menu__overlay" />
+
+        <div className="flex justify-around items-center app__outer">
+          <div className="app__container pt4">
+            <div className="flex flex-column justify-between">
+              <WrapWithRoutingSwitch component={Routes} />
+            </div>
+          </div>
+        </div>
       </ApolloProvider>
     </Router>
   );
 
-  render(<ClientApp component={Menu} />, document.getElementById('menu'));
-
-  hydrate(<ClientApp component={HeaderTitle} />, document.getElementById('header-title'));
-  hydrate(<ClientApp component={LanguagePicker} />, document.getElementById('header-lang-picker'));
-
-  hydrate(<ClientApp component={Routes} />, document.getElementById('main'));
+  hydrate(<ClientApp />, document.getElementById('root'));
 }
 
 onPageLoad(() => {
